3471a39e008c120059adf2409b92891aed6d3555
xianyus <dennis123367@gmail.com>
2020-11-04 14:00:28 +0800

    repo

--- 複數.c
+++ 複數.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <math.h>
-char x;
 int main(int argc, char const *argv[])
 {
-    int a, b, c, d;
+    int a, b, c, d, x;
     float r = 0, o = 0;
     printf("¿é¤Ja¡Bb¡Bc¡Bd­È\n");
     scanf("%d %d %d %d", &a, &b, &c, &d);
     printf("%s%d+%s%d\n", "A1 = ", a, "i", b);
     printf("%s%d+%s%d\n", "A2 = ", c, "i", d);
-    printf("¿é¤J1 = +¡B2 = -¡B3 = *¡B4 = /");
+    printf("¿é¤J1¡B2¡B3¡B4\n");
+    printf("1 = +¡B2 = -¡B3 = *¡B4 = /\n");
     scanf("%d", &x);
+    
 
     switch (x)
     {
-    case '+':
+    case 1:
         r = a + c;
         o = b + d;
         break;
 
-    case '-':
+    case 2:
         r = a - c;
         o = b - d;
         break;
 
-    case '*':
+    case 3:
         r = a * c - b * d;
         o = b * c + a * d;
         break;
 
-    case '/':
-        r = (float)r / (pow(c, 2) + pow(d, 2));
-        o = (float)o / (pow(c, 2) + pow(d, 2));
+    case 4:
+        r = (float)(a * c - b * d) / (pow(c, 2) + pow(d, 2));
+        o = (float)(b * c + a * d) / (pow(c, 2) + pow(d, 2));
         break;
 
     default:
